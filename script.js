95459c4f305223f62c13d80d223dc925cdc3ae23
Quang_bInside <ptquang0968@gmail.com>
2019-05-28 18:46:42 +0700

    Update source

---
+++ script.js
@@ -0,0 +1,30 @@
+﻿
+var jsLogin = {
+
+    // set focus fill
+    setFocusLoginPage: function(){
+        var elemUsername = document.getElementById("inputidentity");
+        var userid = elemUsername.value;
+
+        var elemPassword = document.getElementById("inputpassword");
+        var pass = elemPassword.value;
+
+        if(userid != null && userid != "")
+        {
+          elemUsername.focus();
+          elemPassword.focus();
+        }
+        else
+        {
+          elemUsername.focus();
+        }
+    },
+
+    setFocusModal: function(idModal, idInput){
+         $('#'+idModal).on('shown.bs.modal', function () {
+             // get the locator for an input in your modal. Here I'm focusing on
+             // the element with the id of myInput
+             $('#'+idInput).focus()
+         })
+    }
+};
